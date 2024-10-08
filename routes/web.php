e4de1d7ccba6d8a89aaee2a1c878425114caa22a
Exsy <exsy.dev@gmail.com>
2024-10-08 19:44:30 +0300

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/set-currencies', function () {
+Route::get('/create/invoice', function () {
    $apirone = new \Apirone\ApironeManager();
 
    $apirone = $apirone->setCurrencies([
